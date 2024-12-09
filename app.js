707304d89a8d0d81e2e8f5097d36b31db61b3926
dsj <24hs201@s.bgu.ac.jp>
2024-12-09 14:18:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
             var result = "凶"; // デフォルトの値
 
             if (colour === "肉っしょ" && == "たまご" ) {
-                result = "小吉";
+                result = "中吉";
             } else if (colour === "肉っしょ" && =="大根") {
                 result = "中吉";
             } else if (colour === "肉っしょ" && == "ねぎ") {
@@ -16,11 +16,11 @@
             } else if (colour === "魚かな" && == "ねぎ") {
                 result = "大凶";
             } else if (colour === "麺だね" && == "たまご") {
-                result = "凶";
+                result = "カルボナーラ";
             } else if (colour === "麺だね" && == "大根") {
                 result = "大吉";
             } else if (colour === "麺だね" && == "ねぎ") {
-                result = "中吉";
+                result = "ネギたっぷりラーメン";
             }
 
             document.getElementById('result-output').innerText = "あなた今日の晩御飯は【" + result + "】です";
