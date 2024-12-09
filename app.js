733300dd57c7eee83a5ab7c8303ea8189890b17a
dsj <24hs201@s.bgu.ac.jp>
2024-12-09 13:57:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
         function checkFortune() {
-            var colour = document.getElementById('colour-select').value;
+            var colour = document.getElementById('food-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "肉っしょ" && ) {
+            if (colour === "肉っしょ" && == "たまご" ) {
                 result = "小吉";
-            } else if (colour === "肉っしょ" && ) {
+            } else if (colour === "肉っしょ" && =="") {
                 result = "中吉";
-            } else if (colour === "肉っしょ" && ) {
+            } else if (colour === "肉っしょ" && == "ねぎ") {
                 result = "凶";
-            } else if (colour === "魚かな" && ) {
+            } else if (colour === "魚かな" && == "たまご" ) {
                 result = "中吉";
-            } else if (colour === "魚かな" && ) {
+            } else if (colour === "魚かな" && == "" ) {
                 result = "中吉";
-            } else if (colour === "魚かな" && ) {
+            } else if (colour === "魚かな" && == "ねぎ") {
                 result = "大凶";
-            } else if (colour === "麺だね" && ) {
+            } else if (colour === "麺だね" && == "たまご") {
                 result = "凶";
-            } else if (colour === "麺だね" && ) {
+            } else if (colour === "麺だね" && == "") {
                 result = "大吉";
-            } else if (colour === "麺だね" && ) {
+            } else if (colour === "麺だね" && == "ねぎ") {
                 result = "中吉";
             }
 
