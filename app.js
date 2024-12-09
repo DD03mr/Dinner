79c6ee6acf98b05e5509f7c7f06d25df2a99b218
dsj <24hs201@s.bgu.ac.jp>
2024-12-09 13:48:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,23 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "肉っしょ" && number === 18時) {
+            if (colour === "肉っしょ" && ) {
                 result = "小吉";
-            } else if (colour === "肉っしょ" && number === 20時) {
+            } else if (colour === "肉っしょ" && ) {
                 result = "中吉";
-            } else if (colour === "肉っしょ" && number === 22時) {
+            } else if (colour === "肉っしょ" && ) {
                 result = "凶";
-            } else if (colour === "魚かな" && number === 18時) {
+            } else if (colour === "魚かな" && ) {
                 result = "中吉";
-            } else if (colour === "魚かな" && number === 20時) {
+            } else if (colour === "魚かな" && ) {
                 result = "中吉";
-            } else if (colour === "魚かな" && number === 22時) {
+            } else if (colour === "魚かな" && ) {
                 result = "大凶";
-            } else if (colour === "麺だね" && number === 18時) {
+            } else if (colour === "麺だね" && ) {
                 result = "凶";
-            } else if (colour === "麺だね" && number === 20時) {
+            } else if (colour === "麺だね" && ) {
                 result = "大吉";
-            } else if (colour === "麺だね" && number === 22時) {
+            } else if (colour === "麺だね" && ) {
                 result = "中吉";
             }
 
