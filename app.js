3d69061bc821362cdd09457120b052c3e6c7cfcd
dsj <24hs201@s.bgu.ac.jp>
2024-12-16 13:47:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
         function checkFortune() {
             var colour = document.getElementById('food-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
+            var result = "親子丼"; // デフォルトの値
 
             if (colour === "肉っしょ" && == "たまご" ) {
-                result = "中吉";
+                result = "親子丼";
             } else if (colour === "肉っしょ" && =="大根") {
-                result = "中吉";
+                result = "豚バラ大根";
             } else if (colour === "肉っしょ" && == "ねぎ") {
-                result = "凶";
+                result = "よだれ鶏";
             } else if (colour === "魚かな" && == "たまご" ) {
-                result = "中吉";
+                result = "白身魚フライ~タルタルソースを添えて~";
             } else if (colour === "魚かな" && == "大根" ) {
-                result = "中吉";
+                result = "ぶり大根";
             } else if (colour === "魚かな" && == "ねぎ") {
-                result = "大凶";
+                result = "白身魚の酒蒸し";
             } else if (colour === "麺だね" && == "たまご") {
                 result = "カルボナーラ";
             } else if (colour === "麺だね" && == "大根") {
-                result = "大吉";
+                result = "おろしうどん";
             } else if (colour === "麺だね" && == "ねぎ") {
                 result = "ネギたっぷりラーメン";
             }
