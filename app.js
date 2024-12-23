7a6227b4fc0f0250cd2e819df7ab8fbe87aa8491
dsj <24hs201@s.bgu.ac.jp>
2024-12-23 14:23:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-        function checkFortune() {
-            var colour = document.getElementById('colour-select').value;
+function checkFortune() {
+            var colour = document.getElementById('food-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
+            var result = "親子丼"; // デフォルトの値
+ 
+            if (colour === "肉っしょ" && == "たまご" ) {
+                result = "親子丼";
+            } else if (colour === "肉っしょ" && =="大根") {
+                result = "豚バラ大根";
+            } else if (colour === "肉っしょ" && == "ねぎ") {
+                result = "よだれ鶏";
+            } else if (colour === "魚かな" && == "たまご" ) {
+                result = "白身魚フライ~タルタルソースを添えて~";
+            } else if (colour === "魚かな" && == "大根" ) {
+                result = "ぶり大根";
+            } else if (colour === "魚かな" && == "ねぎ") {
+                result = "白身魚の酒蒸し";
+            } else if (colour === "麺だね" && == "たまご") {
+                result = "カルボナーラ";
+            } else if (colour === "麺だね" && == "大根") {
+                result = "おろしうどん";
+            } else if (colour === "麺だね" && == "ねぎ") {
+                result = "ネギたっぷりラーメン";
             }
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
-        }
+ 
+            document.getElementById('result-output').innerText = "あなた今日の晩御飯は【" + result + "】です";
+        }       
