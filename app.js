40082f89af8d9433833bdb86bfed624306c3b401
dsj <24hs201@s.bgu.ac.jp>
2025-01-20 14:22:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ function checkFortune() {
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "親子丼"; // デフォルトの値
  
-            if (colour === "肉っしょ" && == "たまご" ) {
+            if (colour === "肉っしょ" && food　== "たまご" ) {
                 result = "親子丼";
-            } else if (colour === "肉っしょ" && =="大根") {
+            } else if (colour === "肉っしょ" && food =="大根") {
                 result = "豚バラ大根";
-            } else if (colour === "肉っしょ" && == "ねぎ") {
+            } else if (colour === "肉っしょ" && food == "ねぎ") {
                 result = "よだれ鶏";
-            } else if (colour === "魚かな" && == "たまご" ) {
+            } else if (colour === "魚かな" && food == "たまご" ) {
                 result = "白身魚フライ~タルタルソースを添えて~";
-            } else if (colour === "魚かな" && == "大根" ) {
+            } else if (colour === "魚かな" && food == "大根" ) {
                 result = "ぶり大根";
-            } else if (colour === "魚かな" && == "ねぎ") {
+            } else if (colour === "魚かな" && food == "ねぎ") {
                 result = "白身魚の酒蒸し";
-            } else if (colour === "麺だね" && == "たまご") {
+            } else if (colour === "麺だね" && food == "たまご") {
                 result = "カルボナーラ";
-            } else if (colour === "麺だね" && == "大根") {
+            } else if (colour === "麺だね" && food == "大根") {
                 result = "おろしうどん";
-            } else if (colour === "麺だね" && == "ねぎ") {
+            } else if (colour === "麺だね" && food == "ねぎ") {
                 result = "ネギたっぷりラーメン";
             }
             };
